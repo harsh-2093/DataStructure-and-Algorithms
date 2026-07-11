ead25bcf4c31eaf49ce2fc47dc853968907e0556
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-07-11 19:45:33 +0530

    Add solution for majority-element

---
+++ majority-element/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int element=0;
+        int count=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(count==0)
+            {
+            element=nums[i];
+            //count=1;
+            }
+            if(nums[i]!=element)
+            {
+                count--;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return element;
+    }
+};
