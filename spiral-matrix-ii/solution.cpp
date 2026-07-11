fca6eba675570a48d0ff8eaee96ff246273008a4
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-07-11 19:41:35 +0530

    Add solution for spiral-matrix-ii

---
+++ spiral-matrix-ii/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+
+        int top=0;
+        int left=0;
+        int right=n-1;
+        int bottom=n-1;
+        int count=1;
+        vector<vector<int>>ans(n,vector<int>(n));
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                ans[top][i]=count;
+                count++;
+            }
+            top++;
+            for(int i=top;i<=bottom;i++)
+            {
+                ans[i][right]=count;
+                count++;
+            }
+            right--;
+            if(top<=bottom)
+            {
+                for(int i=right;i>=left;i--)
+                {
+                    ans[bottom][i]=count;
+                    count++;
+                }
+            bottom--;
+            }
+            if(left<=right){
+            for(int i=bottom;i>=top;i--)
+            {
+                ans[i][left]=count;
+                count++;
+            }
+            left++;
+            }
+        }
+        return ans;
+        
+    }
+};
