aba70d17853c1cdba142e3026808f50c8b712e67
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-07-11 19:41:27 +0530

    Add solution for maximum-subarray

---
+++ maximum-subarray/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum=INT_MIN;
+
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            max_sum=max(max_sum,sum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+           
+        }
+        if(max_sum==INT_MIN)return -1;
+        return max_sum;
+
+
+    }
+};
