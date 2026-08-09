d697d425281c0f3042a97242504446d23fc2f7d2
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-08-09 10:31:25 +0530

    Add solution for k-th-smallest-prime-fraction

--- k-th-smallest-prime-fraction/solution.cpp
+++ k-th-smallest-prime-fraction/solution.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         //{fraction,a[i],a[j]}
-        vector<tuple<float,int,int>>Vec;
+        priority_queue<
+        tuple<float,int,int>
+        >pq;
         
 
         for(int i=0;i<arr.size();i++)
         {
-            for(int j=i+1;j<arr.size();j++)
+            for(int j=arr.size()-1;j>i;j--)
             {
-                Vec.push_back({arr[i]/(float)arr[j],arr[i],arr[j]});
+                if(pq.size()<k)
+                {
+                    pq.push({arr[i]/(float)arr[j],arr[i],arr[j]});
+                }
+                else if(pq.size()==k && get<0>(pq.top())>arr[i]/(float)arr[j])
+                {
+                  pq.pop();  
+                  pq.push({arr[i]/(float)arr[j],arr[i],arr[j]});
+                }
+                else
+                {
+                    break;
+                }
+                
             }
         }
-        sort(Vec.begin(),Vec.end());
-
-        int a=get<1>(Vec[k-1]);
-        int b=get<2>(Vec[k-1]);
+        int a=get<1>(pq.top());
+        int b=get<2>(pq.top());
         return {a,b};
     }
 };
