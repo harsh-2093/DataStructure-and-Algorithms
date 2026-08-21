00925273a68d3349dc7ebaa06bd89ba2592a5f82
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-08-21 14:15:51 +0530

    Add solution for lowest-common-ancestor-of-a-binary-tree

---
+++ lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public: 
+    TreeNode* ans =nullptr;
+    int solve(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(root==nullptr)return 0;
+        int cnt=0;
+        if(root->val==p->val || root->val==q->val)
+        {
+            cnt++;
+        }
+
+        int left=solve(root->left,p,q);
+        int right=solve(root->right,p,q);
+
+        if(cnt+left+right>1)
+        {
+            ans=root;
+            return 0;
+        }
+
+        return cnt+left+right;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        solve(root,p,q);
+        return ans;
+    }
+};
