3ded63941b9e66b707abd817e71c065e5d0b92af
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-08-26 21:55:07 +0530

    Add solution for the-k-weakest-rows-in-a-matrix

---
+++ the-k-weakest-rows-in-a-matrix/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pair<int,int>,
+                    vector<pair<int,int>>,
+                    greater<pair<int,int>>
+        >pq;
+
+        for(int i=0;i<mat.size();i++)
+        {
+            int sum=0;
+            for(int j=0;j<mat[0].size();j++)
+            {
+                sum+=mat[i][j];
+            }
+            pq.push({sum,i});
+        }
+        vector<int>ans;
+
+        while(k--)
+        {
+            ans.push_back(get<1>(pq.top()));
+            pq.pop();
+        }
+        return ans;
+    }
+};
