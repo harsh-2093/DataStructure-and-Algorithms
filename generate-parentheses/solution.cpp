8697ff3c660e947f84cc1188b4bb429966d8e7ec
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-08-28 12:41:42 +0530

    Add solution for generate-parentheses

--- generate-parentheses/solution.cpp
+++ generate-parentheses/solution.cpp
@@ -1,37 +1,5 @@
 class Solution {
 public:
-bool isValid(string s) {
-        unordered_map<char,char>mpp{
-            {'(',')'},
-            {'{','}'},
-            {'[',']'},
-        };
-        stack<char>st;
-        for(int i=0;i<s.size();i++)
-        {
-            if(st.empty())
-            {
-                if(mpp.find(s[i])==mpp.end())
-                {
-                    return false;
-                }
-                st.push(s[i]);
-            }
-            else{
-            char back=st.top();
-            if(mpp[back]==s[i])
-            {
-                st.pop();
-            }
-            else
-            {
-                st.push(s[i]);
-            }
-            }
-        }
-
-        return st.empty();
-    }
     vector<string>ans;
     void solve(string curr,int n,int open,int close)
     {
@@ -46,10 +14,12 @@ bool isValid(string s) {
 
         }
 
-
+        if(open<n)
+        {
         curr.push_back('(');
         solve(curr,n,open+1,close);
         curr.pop_back();
+        }
 
         if(close<open){
         curr.push_back(')');
