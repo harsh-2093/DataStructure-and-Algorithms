af13aa84bc65fae7f11cab14f01c7fe826d744d4
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-08-28 13:08:53 +0530

    Add solution for lowest-common-ancestor-of-a-binary-tree

--- lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -9,29 +9,56 @@
  */
 class Solution {
 public: 
-    TreeNode* ans =nullptr;
-    int solve(TreeNode* root, TreeNode* p, TreeNode* q)
-    {
-        if(root==nullptr)return 0;
-        int cnt=0;
+    // TreeNode* ans =nullptr;
+    // int solve(TreeNode* root, TreeNode* p, TreeNode* q)
+    // {
+    //     if(root==nullptr)return 0;
+    //     int cnt=0;
+    //     if(root==p || root==q)
+    //     {
+    //         cnt++;
+    //     }
+
+    //     int left=solve(root->left,p,q);
+    //     int right=solve(root->right,p,q);
+
+    //     if(cnt+left+right>1)
+    //     {
+    //         ans=root;
+    //         return 0;
+    //     }
+
+    //     return cnt+left+right;
+    // }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        if(root==nullptr)return nullptr;
+
         if(root==p || root==q)
         {
-            cnt++;
+            return root;
         }
 
-        int left=solve(root->left,p,q);
-        int right=solve(root->right,p,q);
+        TreeNode* Left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* Right=lowestCommonAncestor(root->right,p,q);
 
-        if(cnt+left+right>1)
+        if(Left!=nullptr && Right!=nullptr)
         {
-            ans=root;
-            return 0;
+            return root;
         }
 
-        return cnt+left+right;
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        solve(root,p,q);
-        return ans;
+        if(Left!=nullptr)
+        {
+            return Left;
+
+        }
+        return Right;
+
+
+
+
+
+
     }
 };
