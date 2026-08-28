cc365837e5bb6a9cdd3c11e36078a5449b3098da
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-08-28 12:40:26 +0530

    Add solution for generate-parentheses

--- generate-parentheses/solution.cpp
+++ generate-parentheses/solution.cpp
@@ -33,31 +33,33 @@ bool isValid(string s) {
         return st.empty();
     }
     vector<string>ans;
-    void solve(string curr,int n)
+    void solve(string curr,int n,int open,int close)
     {
         if(curr.size()==2*n)
         {
-            if(isValid(curr))
+            if( open==close)
             {
                 ans.push_back(curr);
 
             }
-                    return;
+            return;
 
         }
 
 
         curr.push_back('(');
-        solve(curr,n);
+        solve(curr,n,open+1,close);
         curr.pop_back();
 
+        if(close<open){
         curr.push_back(')');
-        solve(curr,n);
+        solve(curr,n,open,close+1);
+        }
         // curr.pop_back();
 
     }
     vector<string> generateParenthesis(int n) {
-        solve("",n);
+        solve("",n,0,0);
         return ans;
     }
 };
