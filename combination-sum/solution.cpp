07124a08b98cee6064bc3bd190259927f456b735
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-09-02 19:15:05 +0530

    Add solution for combination-sum

--- combination-sum/solution.cpp
+++ combination-sum/solution.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>>result;
-    void combination(int sum,vector<int>& candidates,int target,vector<int>&temp,int index)
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>Comb_sum;
+        vector<int>partial;
+        dfs(0,0,candidates,target,partial,Comb_sum);
+        return Comb_sum;
+
+    }
+private:
+    void dfs(int i,int sum,vector<int>& candidates, int target,vector<int>&partial,vector<vector<int>>&Comb_sum)
     {
         if(sum==target)
         {
-            // sort(temp.begin(),temp.end());
-            result.push_back(temp);
+            Comb_sum.push_back(partial);
             return;
         }
+        if(i==candidates.size() || sum >target)return;
 
+        partial.push_back(candidates[i]);
+        dfs(i,sum+candidates[i],candidates,target,partial,Comb_sum);
+        partial.pop_back();
+        dfs(i+1,sum,candidates,target,partial,Comb_sum);
 
-        //base case
-        if(sum>target)
-        {
-            return;
-        }
-
-        
-
-        //all possible combination with duplicates
-        for(int i=index;i<candidates.size();i++)
-        {
-            temp.push_back(candidates[i]);
-            combination(sum+candidates[i],candidates,target,temp,i);
-            temp.pop_back();
-        }
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>temp;
-        int sum=0;
-        combination(sum,candidates,target,temp,0);
-        // vector<vector<int>>res(result.begin(),result.end());
-        return result;
     }
 };
