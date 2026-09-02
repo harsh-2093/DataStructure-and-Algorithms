df79e66a85aba3d983ecad7de473ef9f6cad451b
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-09-02 10:17:54 +0530

    Add solution for construct-uniform-parity-array-i

--- construct-uniform-parity-array-i/solution.cpp
+++ construct-uniform-parity-array-i/solution.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-       if(nums1.size()==1)return true;
-        bool final_odd=true;
-        bool final_even =true;
-        for(int i=0;i<nums1.size();i++)
-        {
-            bool odd=false;
-            bool even =false;
-            for(int j=0;j<nums1.size();j++)
-            {
-                if(i==j)continue;
-                if(nums1[i]%2!=0 || (nums1[i]-nums1[j])%2!=0)
-                {
-                    odd=true;
-                }
-                if(nums1[i]%2==0 || (nums1[i]-nums1[j])%2==0)
-                {
-                    odd=true;
-                }
-            }
-            if(odd==false)final_odd=false;
-            if(even==false)final_even=false;
-        }
-        return (final_odd || final_even);
+        return true;
     }
 };
