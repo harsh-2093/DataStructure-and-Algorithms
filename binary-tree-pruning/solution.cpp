00949c6b9a35d1d3ef8df44006b587df3028da0c
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-09-06 22:11:30 +0530

    Add solution for binary-tree-pruning

--- binary-tree-pruning/solution.cpp
+++ binary-tree-pruning/solution.cpp
@@ -11,32 +11,17 @@
  */
 class Solution {
 public:
-    bool isvalid(TreeNode* root)
-    {
-        if(root==nullptr)return false;
-
-        bool  left=isvalid(root->left);
-        bool right=isvalid(root->right);
-
-        if(root->val==1)return true;
-
-        return (left|| right);
-    }
     TreeNode* pruneTree(TreeNode* root) {
         if(root==nullptr)return nullptr;
-        if(!isvalid(root->left))
-        {
-            root->left=nullptr;
-        }
-        if(!isvalid(root->right))
-        {
-            root->right= nullptr;
-        }
 
-        pruneTree(root->left);
-        pruneTree(root->right);
+        root->left=pruneTree(root->left);
+        root->right=pruneTree(root->right);
 
-        if(!isvalid(root->left)&& !isvalid(root->right) &&root->val==0)return nullptr;
+        if(root->left==nullptr && root->right==nullptr && root->val==0)
+        return nullptr;
+
         return root;
+
+        
     }
 };
