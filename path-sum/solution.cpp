b6ec2f210ff1eac4aa4b142aca97917038990735
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-09-09 09:45:35 +0530

    Add solution for path-sum

---
+++ path-sum/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool result=false;
+    void solve(TreeNode* root,int targetSum,int sum)
+    {
+        if(root==nullptr)return;
+        sum+=root->val;
+        if(root->left==nullptr && root->right==nullptr)
+        {
+            if(sum==targetSum)
+            {
+                result=true;
+
+            }
+            return;
+        }
+
+        solve(root->left,targetSum,sum);
+        solve(root->right,targetSum,sum);
+
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        solve(root,targetSum,0);
+        return result;
+    }
+};
